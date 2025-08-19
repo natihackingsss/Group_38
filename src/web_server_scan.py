e4bee9dec3d7f2b4000b4a59d73087fd2596f1a5
Yafet Seyoum <yafetseyome@gmail.com>
2025-08-19 11:16:18 +0000

    Add files via upload
    Added starter code to the web server vulnerability scanner.

---
+++ src/web_server_scan.py
@@ -0,0 +1,13 @@
+"""
+Owner : yumna-ux
+
+Handles the web vulnerablity scanner.
+"""
+
+class WebVulnerabilityScan:
+    def __init__(self, Target :str):
+        self.URL = URL
+
+    def PortScanner(Target :str):
+        # Continue the web scanner
+        
