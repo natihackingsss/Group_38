c740361983bf868178128bb62b5e4bf618c88ddd
Yafet Seyoum <yafetseyome@gmail.com>
2025-08-19 08:05:20 +0000

    Add files via upload
    Added starter code to the password strength auditor

---
+++ src/password_security.py
@@ -0,0 +1,10 @@
+"""
+Owner: ICBMAY
+
+Handles the functionality of the password strength checker.
+"""
+
+class PassStrength:
+    def __init__(self, pwd: str):
+        self.pwd = pwd
+        # Continue from here
